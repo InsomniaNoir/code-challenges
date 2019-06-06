70a866d295329ec1a329ad30696d09ec0856cb32
InsomniaNoir <jrshafer@mac.com>
2019-06-06 17:34:15 -0500

    Completed other angle

---
+++ other_angle.rb
@@ -0,0 +1,10 @@
+# You are given two angles (in degrees) of a triangle.
+#
+# Write a function to return the 3rd.
+#
+# Note: only positive integers will be tested.
+
+def other_angle(a, b)
+   c = 180 - (a + b)
+   return c
+end
