09ff00a9e7dddec43d83c914c379231ca6106ae4
InsomniaNoir <jrshafer@mac.com>
2019-06-06 17:53:27 -0500

    Reduce but Grow

---
+++ reduce_but_grow.rb
@@ -0,0 +1,14 @@
+# Given a non-empty array of integers, return the result of multiplying the
+# values together in order. Example:
+#
+# [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+def grow(x)
+  x.sort
+  p x.inject(:*)
+end
+
+# p(obj) → obj
+# p(obj1, obj2, ...) → [obj, ...]
+# p() → nil
+# For each object, directly writes obj.inspect followed by a newline to the program’s standard output.
